e19c1f3b8983d48b13d6b1cc347d6277dc69d9c9
Jorge <cokijlmf@gmail.com>
2021-06-23 19:45:09 -0500

    Lector3

--- Lector/Usuario.h
+++ Lector/Usuario.h
@@ -10,19 +10,23 @@ class usuario
 {
 	string nombre;
 	string correo;
-	
+
 public:
 
-	usuario(string nombre, string correo) {}
+	usuario(string nombre, string correo) {
+		this->nombre = nombre;
+		this->correo = correo;
 
-	void ingreso_datos()
-	{
-		cout << "Usuario: ";
-		cin >> nombre;
-
-		cout << "Correo electronico: ";
-		cin >> correo;
 	}
+	// mensaje sobre edicion del documento
+	string get_datos()
+	{
+		return nombre + " edito parte del documento " + correo;
+	}
+
+	void set_name(string nombre) { this->nombre = nombre; }
+
+	void set_correo(string correo) { this->correo = correo; }
 
 	string get_name() { return nombre; }
 
